4208157c927c84b3d837298eddeb8a53df586203
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-06-11 13:05:43 +0600

    Update test_q_serializer.py
    Fix : https://github.com/modlinltd/django-advanced-filters/pull/182#pullrequestreview-1425894485

--- advanced_filters/tests/test_q_serializer.py
+++ advanced_filters/tests/test_q_serializer.py
@@ -1,39 +1,71 @@
 from django.db.models import Q
 from django.test import TestCase
 import json
+import datetime
+
 
 from ..q_serializer import QSerializer
 
 
 class QSerializerTest(TestCase):
     correct_query = {
-        'children': [('test', 1234)],
-        'connector': 'AND',
-        'negated': False,
+        "children": [("test", 1234)],
+        "connector": "AND",
+        "negated": False,
+    }
+    datetime_query = {
+        "children": [("date", datetime.datetime.now().isoformat())],
+        "connector": "AND",
+        "negated": False,
     }
 
     def setUp(self):
         self.s = QSerializer()
         self.query_a = Q(test=1234)
-        self.query_b = Q(another="test")
+        self.date_query = Q(date=datetime.datetime.now().isoformat())
 
     def test_serialize_q(self):
         res = self.s.serialize(self.query_a)
         self.assertEqual(res, self.correct_query)
 
+        date = self.s.serialize(self.date_query)
+        self.assertEqual(date, self.datetime_query)
+
     def test_jsondump_q(self):
         jres = self.s.dumps(self.query_a)
         self.assertJSONEqual(jres, json.dumps(self.correct_query))
 
+        jdate = self.s.serialize(self.date_query)
+        self.assertEqual(jdate, json.dumps(self.datetime_query))
+
     def test_deserialize_q(self):
-        qres = self.s.deserialize({
-            'children': [('test', 1234)],
-            'connector': 'AND',
-            'negated': False,
-            'subtree_parents': []
-        })
+        qres = self.s.deserialize(
+            {
+                "children": [("test", 1234)],
+                "connector": "AND",
+                "negated": False,
+                "subtree_parents": [],
+            }
+        )
         self.assertIsInstance(qres, Q)
 
-        qres = self.s.loads('{"connector": "AND", "negated": false, "children"'
-                            ' :[["test", 1234]], "subtree_parents": []}')
+        qres = self.s.loads(
+            '{"connector": "AND", "negated": false, "children"'
+            ' :[["test", 1234]], "subtree_parents": []}'
+        )
+        self.assertIsInstance(qres, Q)
+
+        dres = self.s.deserialize(
+            {
+                "children": [("date", datetime.datetime.now().isoformat())],
+                "connector": "AND",
+                "negated": False,
+            }
+        )
+        self.assertIsInstance(dres, Q)
+
+        qres = self.s.loads(
+            '{"connector": "AND", "negated": false, "children"'
+            ' :[["date", 2024-06-11T13:02:16.568909]], "subtree_parents": []}'
+        )
         self.assertIsInstance(qres, Q)
